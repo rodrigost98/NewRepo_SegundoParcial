92696c2da7d5514692e3eb46539cefd1c06ad603
rodristu989 <rodristu989@gmail.com>
2019-05-25 13:47:08 -0400

    Commit final

---
+++ RodrigoSantistevan2doParcial/app/app.Android/Interfaces/InstanceLocator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using apo.app.ViewModels;
+using apo.app.ViewModels;
+
+namespace app.Droid.Interfaces
+{
+    public class InstanceLocator
+    {
+        #region Propeties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region constructor
+        public InstanceLocator()
+        this Main = new MainViewModel();
+        #endregion
+
+        #region
+
+        #endregion
+    }
+}
---
+++ RodrigoSantistevan2doParcial/app/app.Android/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿namespace apo.app.ViewModels
+
+{
+    using global::app.Droid.Views;
+    public class MainViewModel
+
+    {
+
+        #region Properties
+
+        public string Token { get; set; }
+
+        public string TokenType { get; set; }
+
+        #endregion
+
+        #region ViewModels
+
+        public NotesPage Login { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+
+        {
+
+            instance = this;
+
+            this.Login = new LoginViewModel();
+
+        }
+
+        #endregion       
+        #region Singleton
+
+        private static MainViewModel instance; public static MainViewModel GetInstance()
+
+        {
+
+            if (instance == null)
+
+            {
+
+                return new MainViewModel();
+
+            }
+
+            return instance;
+
+        }
+
+        #endregion
+    }
+}
